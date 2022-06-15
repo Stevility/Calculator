dd7dad4eda582084689a40c6006c2452211383b3
Steven Fabian <SFabian@student.fullsail.edu>
2022-06-15 16:19:55 -0400

    WxButtons Created

--- Calculator/cMain.cpp
+++ Calculator/cMain.cpp
@@ -2,6 +2,18 @@
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Calculator!", wxPoint(500, 200), wxSize(400, 500))
 {
+	m_Txt1 = new wxTextCtrl(this, wxID_ANY, "0", wxPoint(10, 15), wxSize(350, 30),wxTE_RIGHT);
+	m_Txt1->Disable();
+	num1 = new wxButton(this, wxID_ANY, "1", wxPoint(10, 100), wxSize(60,60));
+	num2 = new wxButton(this, wxID_ANY, "1", wxPoint(10, 100), wxSize(60,60));
+	num3 = new wxButton(this, wxID_ANY, "1", wxPoint(10, 100), wxSize(60,60));
+	num4 = new wxButton(this, wxID_ANY, "1", wxPoint(10, 100), wxSize(60,60));
+	num5 = new wxButton(this, wxID_ANY, "1", wxPoint(10, 100), wxSize(60,60));
+	num6 = new wxButton(this, wxID_ANY, "1", wxPoint(10, 100), wxSize(60,60));
+	num7 = new wxButton(this, wxID_ANY, "1", wxPoint(10, 100), wxSize(60,60));
+	num8 = new wxButton(this, wxID_ANY, "1", wxPoint(10, 100), wxSize(60,60));
+	num9 = new wxButton(this, wxID_ANY, "1", wxPoint(10, 100), wxSize(60,60));
+	num0 = new wxButton(this, wxID_ANY, "1", wxPoint(10, 100), wxSize(60,60));
 }
 
 cMain::~cMain()
--- Calculator/cMain.h
+++ Calculator/cMain.h
@@ -7,5 +7,20 @@ public:
 	cMain();
 	~cMain();
 
+public:
+	//buttons for number
+	wxButton* num1 = nullptr;
+	wxButton* num2 = nullptr;
+	wxButton* num3 = nullptr;
+	wxButton* num4 = nullptr;
+	wxButton* num5 = nullptr;
+	wxButton* num6 = nullptr;
+	wxButton* num7 = nullptr;
+	wxButton* num8 = nullptr;
+	wxButton* num9 = nullptr;
+	wxButton* num0 = nullptr;
+
+	//Text Box
+	wxTextCtrl* m_Txt1 = nullptr;
 };
 
