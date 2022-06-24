39b63b36819349ce58873595d1f40f52d41fcca8
Steven Fabian <SFabian@student.fullsail.edu>
2022-06-24 19:17:33 -0400

    Added the (+,-,*,/) commands

--- Calculator/IBaseCommand.h
+++ Calculator/IBaseCommand.h
@@ -1,5 +1,16 @@
 #pragma once
 class IBaseCommand
 {
+public: 
+	virtual double execute() = 0;
+
+private:
+	double AddCommand();
+	double SubtractCommand();
+	double MultiplyCommand();
+	double DivideCommand();
+	double execute();
+	double x = 0;
+	double y = 0;
 };
 
