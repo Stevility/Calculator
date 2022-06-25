cdb94ab7c32060c9390135338ed04fbab22550c5
Steven Fabian <SFabian@student.fullsail.edu>
2022-06-24 21:54:17 -0400

    Redid Test folder

--- CalculatorTests/ButtonFactoryTests.cpp
+++
@@ -1,1 +0,0 @@
-#include "ButtonFactoryTests.h"
---
+++ Calculator_Test/CalculatorProcessorTest.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalculatorTest
+{
+	TEST_CLASS(CalculatorProcessorTests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
